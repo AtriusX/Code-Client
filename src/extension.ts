1f5ad4c82a9952887b981d26e6fb5a1f8fc56218
Atrius <atriusedm@gmail.com>
2019-07-22 17:57:25 -0400

    Gist creation command

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,8 @@
-import    { window, commands, ExtensionContext, workspace }  from 'vscode';
-import    { github } 							  			 from './github';
-import * as OctoKit 							  			 from '@octokit/rest';
-import    { Account, AccountType } 							 from './Account';
-import { start } from 'repl';
-					
+import    { window, commands, ExtensionContext, workspace, Range } from 'vscode';
+import    { github } 							  			 	   from './github';
+import * as OctoKit 							  				   from '@octokit/rest';
+import    { Account, AccountType } 							 	   from './Account';
+
 let cli: OctoKit;
 
 // Extension configuration
@@ -179,9 +178,61 @@ export function activate(context: ExtensionContext) {
 			window.showErrorMessage('Delete operation aborted');
 		}
 	});
+
+	install(context, 'create-gist', async () => {
+		let active = window.activeTextEditor;
+
+		if (!active) {
+			window.showInformationMessage('This command can only be ran from text editors');
+			return;
+		}
+
+		let type = await window.showQuickPick(['File', 'Selection']);
+
+		
+		
+		let visible = await window.showQuickPick(['Public', 'Private'], {
+			placeHolder: 'Account visibility'
+		});
+		
+		let doc = active.document;
+		let file = doc.fileName.split('\\');
+		let data = { name: file[file.length - 1], data: '' };
+		switch (type) {
+			case 'File':
+				data.data = doc.getText();
+				break;
+			case 'Selection':
+				let sel = active.selection;
+				let range = new Range(sel.start, sel.end);
+				data.data = doc.getText(range);
+				break;
+		}
+	
+		let desc = await window.showInputBox({
+			prompt: 'Enter a description for your Gist'
+		});
+
+		if (data.data.trim().length === 0) {
+			window.showInformationMessage('Cannot create Gist without any data');
+			return;
+		}
+
+		github.currentAccount.login().gists.create({
+			description: desc,
+			public: visible === 'Public' ? true : false,
+			files: {
+				[data.name!]: {
+					content: data.data
+				}
+			}
+		}).then(() => {
+			window.showInformationMessage(`Created Gist '${data.name}'`);
+		});
+	});
 }
 
-export function deactivate() {}
+// export function deactivate() {}
 
 async function install(
 	context: ExtensionContext, 
