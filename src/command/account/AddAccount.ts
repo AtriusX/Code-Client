4a2b0a9c64dc531eccc27e2ad1501da039e70939
Atrius <atriusedm@gmail.com>
2019-07-23 14:39:47 -0400

    Update to AddAccount

--- src/command/account/AddAccount.ts
+++ src/command/account/AddAccount.ts
@@ -9,20 +9,19 @@ export class AddAccount implements Command {
     async run(): Promise<void> {
         let user = AccountType.USER, token = AccountType.TOKEN;
 		let type = await Input.booleanChoice(
-			user, token, 'User Type'
+			user, token, 'User Type', true
 		);
 		
 		let isuser = type === true;
-
 		let name = await Input.input(
-			`Please type in the ${isuser ? 'account username.' : 'token name.'}`, type !== undefined
+			`Please type in the ${isuser ? 'account username.' : 'token name.'}`, type
 		);
-
+		
 		let key = await Input.input(
 			`Please type in the ${isuser ? 'account password.' : 'token.'}`, name, true
 		);
 
-		if (type && name && key) {
+		if (name && key) {
 			Config.addAccount(
 				new Account(isuser ? user : token, name, key)
 			);
