d27fc559f6fb98dd0197e003c89f1878b4383331
Atrius <atriusedm@gmail.com>
2019-07-24 11:10:57 -0400

    Revert use of sequences in CreateGist

--- src/command/gist/CreateGist.ts
+++ src/command/gist/CreateGist.ts
@@ -1,5 +1,5 @@
 import { Command } from "..";
-import { window, Range } from "vscode";
+import { window, Range, Position } from "vscode";
 import { github } from "../../github";
 import { Config } from "../../config";
 import { Input } from "../../util";
@@ -8,60 +8,48 @@ export class CreateGist implements Command {
     command: string = 'create-gist';   
     
     async run(): Promise<void> {
-    if (!window.activeTextEditor) {
+    let active = window.activeTextEditor;
+    if (!active) {
       window.showInformationMessage('This command can only be ran from text editors');
       return;
     }
 
-    let active = window.activeTextEditor;
-    let data: { name: string, data: string };
-    let [type, visible, desc] = await Input.sequence([
-      {
-        type: 'pick',
-        inputs: ['File', 'Selection'],
-      },
-      {
-        type: 'pick',
-        text: 'Account visibility',
-        inputs: ['Public', 'Private'],
-        action: () => {
-          let doc = active.document;
-          let file = doc.fileName.split('\\');
-          data = { name: file[file.length - 1], data: '' };
-          switch (type) {
-            case 'File':
-              data.data = doc.getText();
-              break;
-            case 'Selection':
-              let sel = active.selection;
-              let range = new Range(sel.start, sel.end);
-              data.data = doc.getText(range);
-              break;
-          }
-        }
-      },
-      {
-        type: 'text',
-        text: 'Enter a description for your Gist',
-        action: () => {
-          if (data.data.trim().length === 0) {
-            window.showInformationMessage('Cannot create Gist without any data');
-            return;
-          }
+    let type = await Input.booleanChoice('File', 'Selection');
+    if (type === undefined) { return; }
 
-          Config.currentAccount()!.login().gists.create({
-            description: desc,
-            public: visible === 'Public' ? true : false,
-            files: {
-              [data.name]: {
-                content: data.data
-              }
-            }
-          }).then(() => {
-            window.showInformationMessage(`Created Gist '${data.name}'`);
-          });
+    let visible = await Input.booleanChoice('Public', 'Private', 'Account visibility');
+    if (visible === undefined) { return; }
+
+    let doc = active.document;
+    let file = doc.fileName.split('\\');
+    let data = { name: file[file.length - 1], data: '' };
+    let sel = active.selection;
+    let text = type ? doc.getText() : new Range(sel.start, sel.end); 
+    
+    if (type) {
+      data.data = text as string; 
+    } else {
+      data.data = doc.getText(text as Range);
+    }
+
+    if (data.data.trim().length === 0) {
+      window.showInformationMessage('Cannot create Gist without any data');
+      return;
+    }
+
+    let desc = await Input.input('Enter a description for your Gist');
+    if (desc === undefined) { return; }
+    
+    github.currentAccount.login().gists.create({
+      description: desc,
+      public: visible ? true : false,
+      files: {
+        [data.name!]: {
+          content: data.data
         }
       }
-    ]);
+    }).then(() => {
+      window.showInformationMessage(`Created Gist '${data.name}'`);
+    });
   }
 }
